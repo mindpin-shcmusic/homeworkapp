bbca0abc1861c61e917a48e01d384244a09acd2d
luffycn <kingla_pei@163.com>
2012-03-19 17:40:28 +0800

    Change the submitted_students and unsubmitted_students functions

--- app/models/homework.rb
+++ app/models/homework.rb
@@ -2,26 +2,17 @@ class Homework < ActiveRecord::Base
   # --- 模型关联
   belongs_to :user, :class_name => 'User', :foreign_key => 'creator_id'
   has_many :homework_assigns
+  
+  # 未提交作业学生
+  has_many :unsubmitted_students, :through => :homework_assigns, :source => :user, :conditions => ['is_submit = ?', false]
+  # 已提交作业学生
+  has_many :submitted_students, :through => :homework_assigns, :source => :user, :conditions => ['is_submit = ?', true]
+  
   accepts_nested_attributes_for :homework_assigns
   
   # --- 校验方法
   validates :title, :content, :presence => true
   
-  # 尚未提交作业
-  def unsubmitted_students
-    HomeworkAssign.find(
-      :all,
-      :conditions => {:is_submit => false, :homework_id => self.id}
-    ).map{|x| x.student }
-  end
-  
-  # 已提交作业
-  def submitted_students
-    HomeworkAssign.find(
-      :all,
-      :conditions => {:is_submit => true, :homework_id => self.id}
-    ).map{|x| x.student }
-  end
   
   # --- 给其他类扩展的方法
   module UserMethods
--- app/models/homework_assign.rb
+++ app/models/homework_assign.rb
@@ -1,7 +1,7 @@
 class HomeworkAssign < ActiveRecord::Base
   # --- 模型关联
   belongs_to :homework, :class_name => 'Homework'
-  belongs_to :student, :class_name => 'User', :foreign_key => 'creator_id'
+  belongs_to :user, :class_name => 'User', :foreign_key => 'creator_id'
   
   # --- 校验方法
   validates :student, :presence => true

d17760ccbe6b0883e5a222451aeb10feccc2c9df
luffycn <kingla_pei@163.com>
2012-03-19 17:21:08 +0800

    Refactor the homework assignment query

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -5,12 +5,12 @@ class StudentController < ApplicationController
 
   def show
     @homework = Homework.find(params[:id])
-    @student = HomeworkAssign.find_by_creator_id_and_homework_id(current_user.id, params[:id])
+    @student = current_user.homework_assigns.find_by_homework_id(params[:id])
     @homework_assign = HomeworkAssign.new
   end
 
   def create
-    @homework_assign = HomeworkAssign.find_by_creator_id_and_homework_id(current_user.id, params[:homework_assign][:homework_id])
+    @homework_assign = current_user.homework_assigns.find_by_homework_id(params[:homework_assign][:homework_id])
     @homework_assign.content = params[:homework_assign][:content]
     @homework_assign.is_submit = true
     return redirect_to :back if @homework_assign.save

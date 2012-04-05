ed2e8395b03e35e0152b9ac3c554de6ef5f7dad0
luffycn <kingla_pei@163.com>
2012-04-05 17:29:31 +0800

    Student homework

--- app/models/homework_assign.rb
+++ app/models/homework_assign.rb
@@ -9,9 +9,12 @@ class HomeworkAssign < ActiveRecord::Base
   # --- 给其他类扩展的方法
   module UserMethods
     def self.included(base)
-      base.has_many :homework_assigns, :foreign_key => :creator_id
-      base.has_many :undeadline_homeworks, :through => :homeworks, :class_name => 'HomeworkAssign', :source => :homework_assigns, :conditions => ['deadline > ?', Time.new.strftime("%Y-%m-%d %H:%M:%S")]
-      base.has_many :deadline_homeworks,:through=>:homework_assigns,:source=>:homework, :conditions => ['homeworks.deadline <= ?', Time.now] 
+      base.has_many :homeworks, :through=>:homework_assigns, :source=>:homework
+      base.has_many :deadline_homeworks,:through=>:homework_assigns,
+                    :source=>:homework, :conditions => ['homeworks.deadline > ?', Time.now] 
+      
+      base.has_many :undeadline_homeworks,:through=>:homework_assigns,
+                    :source=>:homework, :conditions => ['homeworks.deadline <= ?', Time.now] 
       
       base.send(:include, InstanceMethods)
     end

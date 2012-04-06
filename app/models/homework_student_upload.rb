a09c98b6cb7864a4cff7ddf37bb19228d6809ea3
luffycn <kingla_pei@163.com>
2012-04-06 15:07:45 +0800

    add student attachement download url

--- app/models/homework_student_upload.rb
+++ app/models/homework_student_upload.rb
@@ -1,11 +1,14 @@
 class HomeworkStudentUpload < ActiveRecord::Base
+  # --- 模型关联
   belongs_to :creator, :class_name => 'User', :foreign_key => 'creator_id'
   belongs_to :homework_student_attachement, :class_name => 'HomeworkStudentAttachement', 
              :foreign_key => 'attachement_id'
 
              
   # 学生提交物路径
-  has_attached_file :attachement, :path => "/web/2012/:class/:attachment/:id/:style/:basename.:extension"
+  has_attached_file :attachement, :path => "/web/2012/:class/:attachment/:id/:style/:basename.:extension",
+                    :url => "/web/2012/:class/:attachment/:id/:style/:basename.:extension"
+  
   
   def self.find_current(creator_id, attachement_id)
     HomeworkStudentUpload.where(:creator_id => creator_id, :attachement_id => attachement_id).first
